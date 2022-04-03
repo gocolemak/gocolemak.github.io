983828cef4f2b49c935842ac4cb7cb3815082177
victor <victorfengming@163.com>
2022-04-03 22:20:28 +0800

    init commit

---
+++ docs/subapp/sub-react/precache-manifest.66a4fbc08d711f37cfdab877a98f6360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3027c43c20b39aa50e69fc7200d79279",
+    "url": "/subapp/sub-react/index.html"
+  },
+  {
+    "revision": "a24cb8e9669bf5ba5f29",
+    "url": "/subapp/sub-react/static/css/main.fd2227a1.chunk.css"
+  },
+  {
+    "revision": "54144ee678bbc4adceff",
+    "url": "/subapp/sub-react/static/js/2.552a0d8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/subapp/sub-react/static/js/2.552a0d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a24cb8e9669bf5ba5f29",
+    "url": "/subapp/sub-react/static/js/main.7daf5a7a.chunk.js"
+  },
+  {
+    "revision": "935ca0651b0109d01f76",
+    "url": "/subapp/sub-react/static/js/runtime-main.09e55165.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/subapp/sub-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
